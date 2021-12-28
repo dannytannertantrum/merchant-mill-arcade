a89b88b6796602d2119d6bac1cdf725b6dc4550f
Jaime Farnan <info@jaimefarnan.com>
2021-12-28 16:34:38 -0500

    (api) blow away built folder and update gitignore to not include these files

--- api/built/games.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var games = [
-    { id: '1', title: 'Frogger', description: 'Costanza\'s favorite' },
-    { id: '2', title: 'Attack From Mars', description: 'A classic' },
-    { id: '3', title: 'Burger Time', description: 'Peter Pickle the jerk' }
-];
-exports.default = games;
--- api/built/scores.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var scores = [
-    { id: '1', initials: 'GLC', score: 2238934002, game: 'Frogger' },
-    { id: '2', initials: 'GLC', score: 220032912, game: 'Attack From Mars' },
-    { id: '3', initials: 'GLC', score: 11920933, game: 'Burger Time' }
-];
-exports.default = scores;
