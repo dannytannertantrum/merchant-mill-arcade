6cc56fe9f5ea07eb57580327ed7535b4a8bf2d2f
Jaime Farnan <jaime-farnan@pluralsight.com>
2022-03-29 20:44:08 -0400

    (api) Create first test suite for utility functions and add test watch to package json

---
+++ api/src/routes/__tests__/utilities.unit.ts
@@ -0,0 +1,39 @@
+import { constructSlug } from '../utilities'
+
+describe('Construct Slug', () => {
+
+    test('should trim whitespace and replace with hyphens', () => {
+        const trimStringBefore = 'dwayne the rock johnson     '
+        const trimStringAfter = 'dwayne-the-rock-johnson'
+
+        expect(constructSlug(trimStringBefore)).toEqual(trimStringAfter)
+    })
+
+    test('should collapse extra hyphens', () => {
+        const collapseHyphensBefore = 'i am homer-----simpson'
+        const collapseHyphensAfter = 'i-am-homer-simpson'
+
+        expect(constructSlug(collapseHyphensBefore)).toEqual(collapseHyphensAfter)
+    })
+
+    test('should remove character accents', () => {
+        const removeAccentsBefore = 'él va mañana'
+        const removeAccentsAfter = 'el-va-manana'
+
+        expect(constructSlug(removeAccentsBefore)).toEqual(removeAccentsAfter)
+    })
+
+    test('should remove any characters that are not letters, numbers or hyphens', () => {
+        const removeInvalidCharsBefore = '\n\t hello there-number 9!'
+        const removeInvalidCharsAfter = 'hello-there-number-9'
+
+        expect(constructSlug(removeInvalidCharsBefore)).toEqual(removeInvalidCharsAfter)
+    })
+
+    test('should change letters to lowercase', () => {
+        const lowercaseBefore = 'Xentrex-it WORKS'
+        const lowercaseAfter = 'xentrex-it-works'
+
+        expect(constructSlug(lowercaseBefore)).toEqual(lowercaseAfter)
+    })
+})
