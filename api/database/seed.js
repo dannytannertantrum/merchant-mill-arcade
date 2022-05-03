6cd9b63e9a803864aa70d9c557642e4a72ca59c0
Jaime Farnan <jaime-farnan@pluralsight.com>
2022-05-03 18:51:58 -0400

    (api) Add seed CSVs and update seed.js

--- api/database/seed.js
+++ api/database/seed.js
@@ -1,27 +1,34 @@
 const { createPool, sql } = require('slonik')
+const { readFile } = require('fs/promises')
+const path = require('path')
 
 
-const gameUuids = [
-    '3d907a0e-8793-4061-b170-353f885e6e10',
-    'e00a250f-8155-47b8-bf8b-61dc193784d0',
-    'e2f192f1-3e5d-43d2-8f84-c133d799a9a5'
-]
-const initials = ['glc', 'jss', 'cak', 'erb']
-const scoresForInsert = [443656, 358246, 838559, 454114, 247629, 719800, 662903, 103299, 865354, 294080]
-const scoreUuids = [
-    '98b72df5-d470-45c5-a011-02444e159a07',
-    '19104ce6-5955-43df-ac8a-9f80e0e0fe24',
-    '4b458f33-8329-48c0-a960-e44192c2a28e',
-    'ccdd41ee-1d69-4023-b6ff-53134ea2265f',
-    '8bbc0912-cff4-4ed8-9e87-8ce3165bc797',
-    '34c8ea93-247a-4497-9bc9-dcc978a76b50',
-    'ae8f163b-9efd-4671-8ead-8db7347e5c08',
-    'e14f3788-3c21-4631-820f-05362a8d82b7',
-    '1be0a6e0-4a2d-438f-8f31-926248e08694',
-    'a0124c0d-089b-488a-809a-2685a1af1235'
-]
+const gamesCSV = path.join(__dirname, './seedDataGames.csv')
+const scoresCSV = path.join(__dirname, './seedDataScores.csv')
 
-const date = new Date().toISOString()
+const extractCSVData = async (filePath) => {
+    const data = await readFile(filePath, { encoding: 'utf-8' }).catch(reason => {
+        throw new Error(`ERROR EXTRACTING CSV DATA: ${reason}`)
+    })
+
+    // We can ignore the first line, which is simply the header
+    const rawData = data.split(/\r?\n/).splice(1)
+
+    const dateCheckRegex = /[0-9]{4}-[0-9]{1,2}-[0-9]{1,2}/
+
+    const formattedForInsert = rawData.map(val => {
+        const formatEntires = val.split(',').map(val => {
+            if (val === '') return null
+            if (dateCheckRegex.test(val)) return new Date(val).toISOString()
+            if (val === 't') return true
+            if (val === 'f') return false
+            return val
+        })
+        return formatEntires
+    })
+
+    return formattedForInsert
+}
 
 const CONNECTION_STRING = process.env.NODE_ENV === 'TEST'
     ? process.env.TEST_POSTGRES_CONNECTION_STRING
@@ -29,6 +36,10 @@ const CONNECTION_STRING = process.env.NODE_ENV === 'TEST'
 
 async function seedAllData() {
     const slonik = createPool(CONNECTION_STRING)
+
+    const gameData = await extractCSVData(gamesCSV)
+    const scoreData = await extractCSVData(scoresCSV)
+
     try {
         await slonik.query(sql`
             DELETE FROM scores;
@@ -37,34 +48,26 @@ async function seedAllData() {
 
         // We need to split this up from the DELETE commands because when using parameters with Postgres,
         // We can only run a single query https://github.com/lib/pq/issues/928#issuecomment-575254724
-        await slonik.query(sql`
-            INSERT INTO
-                games (id, description, is_deleted, slug, title, created_at)
-            VALUES
-                (${gameUuids[0]}, 'A Costanza classic', FALSE, 'frogger', 'Frogger', ${date}::timestamptz),
-                (${gameUuids[1]}, 'Where''s Will Smith when you need him?', FALSE, 'attack-from-mars', 'Attack from Mars', ${date}::timestamptz),
-                (${gameUuids[2]}, 'Peter Pickle the jerk', FALSE, 'burger-time', 'Burger Time', ${date}::timestamptz);
-        `)
-
-        const gameData = await slonik.query(sql`
-            SELECT id from games
-        `)
-
-        const gameIds = gameData.rows.map(row => row.id)
-
-        for (let i = 0; i < 10; i++) {
-            const gameFK = gameIds[i % gameIds.length]
-            const initialsForInsert = initials[i % initials.length]
-
+        for (let i = 0; i < gameData.length; i++) {
             await slonik.query(sql`
                 INSERT INTO
-                    scores (id, game, initials, is_deleted, score, created_at)
+                    games (id, description, image_url, is_deleted, slug, title, created_at, updated_at)
                 VALUES
-                    (${scoreUuids[i]}, ${gameFK}, ${initialsForInsert}, FALSE, ${scoresForInsert[i]}, ${date}::timestamptz);
+                    (${gameData[i][0]}, ${gameData[i][1]}, ${gameData[i][2]}, ${gameData[i][3]}, ${gameData[i][4]}, ${gameData[i][5]}, ${gameData[i][6]}, ${gameData[i][7]});
             `)
         }
-    } catch (err) {
-        console.log('Slippery Pete needs help!', err)
+
+        for (let i = 0; i < scoreData.length; i++) {
+            await slonik.query(sql`
+                INSERT INTO
+                    scores (id, game, initials, is_deleted, score, created_at, updated_at)
+                VALUES
+                    (${scoreData[i][0]}, ${scoreData[i][1]}, ${scoreData[i][2]}, ${scoreData[i][3]}, ${scoreData[i][4]}, ${scoreData[i][5]}, ${scoreData[i][6]});
+            `)
+        }
+
+    } catch (reason) {
+        console.log('Slippery Pete needs help!', reason)
     } finally {
         await slonik.end();
     }
