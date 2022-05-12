d752a4cc097c09a3247bdabd247898ac96a02f0f
Jaime Farnan <jaime-farnan@pluralsight.com>
2022-05-12 13:49:42 -0400

    (root, client) Set up TS support for frontend/parcel

--- client/src/index.js
+++ client/src/index.js
@@ -2,5 +2,5 @@ import ReactDOM from 'react-dom'
 import { App } from './App'
 
 
-const app = document.getElementById('arcade');
-ReactDOM.render(<App />, app);
+const app = document.getElementById('arcade')
+ReactDOM.render(<App />, app)
